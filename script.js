cc46fbd564e40d88fd040670fd862f2133931637
trinitydevs <trinitynascimento@gmail.com>
2024-06-09 23:39:48 -0300

    site finalizado

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelector('.carousel-control.prev').addEventListener('click', function() {
+    // Mover para a imagem anterior
+    var currentTransform = parseInt(document.querySelector('.carousel-inner').style.transform.replace('translateX(', '').replace('%)', '')) || 0;
+    currentTransform += 100;
+    if (currentTransform > 0) currentTransform = -200;
+    document.querySelector('.carousel-inner').style.transform = `translateX(${currentTransform}%)`;
+});
+
+document.querySelector('.carousel-control.next').addEventListener('click', function() {
+    // Mover para a próxima imagem
+    var currentTransform = parseInt(document.querySelector('.carousel-inner').style.transform.replace('translateX(', '').replace('%)', '')) || 0;
+    currentTransform -= 100;
+    if (currentTransform < -200) currentTransform = 0;
+    document.querySelector('.carousel-inner').style.transform = `translateX(${currentTransform}%)`;
+});
+
+$('.owl-prev').remove(); // Remove o botão "anterior"
+$('.owl-next').remove(); // Remove o botão "próximo"
